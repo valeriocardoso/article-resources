32a4b80cd5b3316d6907705ea9f252d33f435a45
Valério Cardoso <valeriocardoso@outlook.com.br>
2020-07-22 22:12:17 -0300

    function prepare image finished

--- ML/Deploy-ML-Models/modelserver/main.py
+++ ML/Deploy-ML-Models/modelserver/main.py
@@ -3,9 +3,41 @@ Model servel scripts
 
 """
 
-
 import base64
 import json
-import os 
+import os
 import sys
 import time
+import numpy as np
+from PIL import Image
+import redis
+
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications import imagenet_utils
+
+from fastapi import FastAPI, File, HTTPException
+from starlette.requests import Request
+
+app = FastAPI()
+
+db = redis.StrictRedis(host=os.environ.get("REDIS_HOST"))
+
+CLIENT_MAX_TRIES = int(os.environ.get("CLIENT_MAX_TRIES"))
+
+
+def prepare_image(image, target):
+    # if the image mode is not RGB, convert it
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+
+    image = image.resize(target)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    image = imagenet_utils.preprocess_input(image)
+
+    return image
+
+
+@app.get("/")
+def index():
+    return "Hello World!"
